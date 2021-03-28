b0b0f837aef4ee6643963cb26bd7074edc5d0427
Abdullahi Ahmed <abhas955@gmail.com>
2021-03-28 17:11:51 -0400

    stringArr

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
 console.log("Hello World!\n");
+console.log("Hi, I like Git!");
 
+var names = ["ABdullahi", "Abdirahman", "Abdirahim", "Fayou"];
+
+for (x in names) {
+    console.log(names[x]);
+}
